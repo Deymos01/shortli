c592d0c212d3b196ed77bcf4968c93f4522af766
Deymos01 <essenion@gmail.com>
2025-08-27 21:45:23 +0300

    chore: implement custom logger for local env

--- cmd/shortli/main.go
+++ cmd/shortli/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"os"
 	"shortli/internal/config"
+	mwLogger "shortli/internal/http-server/middleware/logger"
+	"shortli/internal/lib/logger/handlers/slogpretty"
 	"shortli/internal/lib/logger/sl"
 	"shortli/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
 )
 
 const (
@@ -28,15 +33,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = storage.DeleteURL("example")
-	if err != nil {
-		log.Error("failed to delete url", sl.Err(err))
-		os.Exit(1)
-	}
-
 	_ = storage
 
-	// TODO: init router: chi, "chi render"
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	// TODO: run server
 }
@@ -46,9 +51,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -60,4 +63,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
